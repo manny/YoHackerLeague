211e1fe3b4fb77e0eb82779f2369e74725fc2b98
Manuel Lopez <mjl288@scarlemail.rutgers.edu>
2014-06-28 02:05:40 -0400

    test yo_all

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python
+
+from flask import json, jsonify
+from pymongo import MongoClient
+import requests
+import os
+
+
+client = MongoClient()
+db = client['yo']
+hackathons = db['hackathons']
+
+api_key = os.environ['API_KEY']
+
+def yo_all(token):
+    requests.post("http://api.justyo.co/yoall/", 
+                  data={'api_token': token})
+yo_all(api_key)
