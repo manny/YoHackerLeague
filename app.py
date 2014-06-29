8db48aaef1925008d71d23a32315c543accb2e06
Manuel Lopez <mjl288@scarlemail.rutgers.edu>
2014-06-28 20:30:15 -0400

    update events function

--- app.py
+++ app.py
@@ -15,4 +15,17 @@ api_key = os.environ['API_KEY']
 def yo_all(token):
     requests.post("http://api.justyo.co/yoall/", 
                   data={'api_token': token})
+
+def update_events():
+    r = requests.get(
+        "http://hackerleague.org/api/v1/hackathons.json")
+    for event in r.json():
+        if event.get('location').get('state') is "New York":
+            update_collection(event)
+            print 
+
+def update_collection(event):
+    
+
+
 yo_all(api_key)
